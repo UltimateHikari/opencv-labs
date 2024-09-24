635dc7d0965583080e9957cbb460eeedbf36cbba
Andrey Rudometov <a.rudometov@postgrespro.ru>
2024-09-24 22:24:06 +0700

    day2 wip: missing gamma & histograms

--- 4_corrections/corrections.py
+++ 4_corrections/corrections.py
@@ -1,4 +1,5 @@
 # source: https://docs.opencv.org/3.1.0/d9/dc8/tutorial_py_trackbar.html
+# https://www.geeksforgeeks.org/changing-the-contrast-and-brightness-of-an-image-using-python-opencv/
 import cv2
 import numpy as np
 import sys
@@ -6,98 +7,53 @@ import sys
 def nothing(x):
 	pass
 
-TBMAX = 255
+CMAX = 127
+BMAX = 255
 WNAME = 'Brightness, Contrast, Gamma'
 
 def bsg(event=0): 
-	b = cv2.getTrackbarPos('brightness', WNAME) - TBMAX
-	s = cv2.getTrackbarPos('contrast', WNAME) - TBMAX
-	g = cv2.getTrackbarPos('gamma', WNAME) - TBMAX
+	b = cv2.getTrackbarPos('brightness', WNAME)
+	c = cv2.getTrackbarPos('contrast', WNAME)
+	g = cv2.getTrackbarPos('gamma', WNAME)
 	effect = controller(img, b, c, g) 
-	cv2.imshow('Effect', effect) 
+	cv2.imshow(WNAME, effect)
   
 def controller(img, brightness=255, 
-               contrast=127): 
-    
-    brightness = int((brightness - 0) * (255 - (-255)) / (510 - 0) + (-255)) 
-  
-    contrast = int((contrast - 0) * (127 - (-127)) / (254 - 0) + (-127)) 
-  
-    if brightness != 0: 
-  
-        if brightness > 0: 
-  
-            shadow = brightness 
-  
-            max = 255
-  
-        else: 
-  
-            shadow = 0
-            max = 255 + brightness 
-  
-        al_pha = (max - shadow) / 255
-        ga_mma = shadow 
-  
-        cal = cv2.addWeighted(img, al_pha,  
-                              img, 0, ga_mma) 
-  
-    else: 
-        cal = img 
-  
+               contrast=127, gamma = 0):
+
+	brightness = int((brightness - 0) * (255 - (-255)) / (510 - 0) + (-255)) 
+	contrast = int((contrast - 0) * (127 - (-127)) / (254 - 0) + (-127))
+	if brightness != 0: 
+		if brightness > 0: 
+			shadow = brightness 
+			max = BMAX
+		else: 
+			shadow = 0
+			max = BMAX + brightness 
+		al_pha = (max - shadow) / 255
+		ga_mma = shadow 
+		cal = cv2.addWeighted(img, al_pha, img, 0, ga_mma) 
+	else: 
+		cal = img 
+
 	if contrast != 0: 
 		Alpha = float(131 * (contrast + 127)) / (127 * (131 - contrast)) 
 		Gamma = 127 * (1 - Alpha) 
+		cal = cv2.addWeighted(cal, Alpha, cal, 0, Gamma) 
+	return cal 
 
-		cal = cv2.addWeighted(cal, Alpha,  
-                              cal, 0, Gamma) 
-  
-    return cal 
-  
 if __name__ == '__main__': 
 	img = cv2.imread(sys.argv[1])
 	
-	cv2.namedWindow(WNAME) 
+	cv2.namedWindow(WNAME)
+
+	cv2.imshow(WNAME, img)
 	
-	cv2.createTrackbar('brightness', WNAME, TBMAX*2, TBMAX*4, bsg)
-	cv2.createTrackbar('contrast', WNAME, TBMAX, TBMAX*2, bsg)
-	cv2.createTrackbar('gamma', WNAME, 0, TBMAX*2, nothing)
+	cv2.createTrackbar('brightness', WNAME, BMAX, BMAX*2, bsg)
+	cv2.createTrackbar('contrast', WNAME, CMAX, CMAX*2, bsg)
+	cv2.createTrackbar('gamma', WNAME, 0, BMAX*2, nothing)
      
- 	bsg(0)
+	bsg(0)
 
-cv2.waitKey(0) 
-
-
-
-
-
-
-
-
-
-
-
-
-
-cv2.createTrackbar('brightness', WNAME, 0, TBMAX*2, nothing)
-cv2.createTrackbar('contrast', WNAME, 0, TBMAX*2, nothing)
-cv2.createTrackbar('gamma', WNAME, 0, TBMAX*2, nothing)
-
-cv2.imshow('image',img)
-
-
-while(1):
-	cv2.imshow('image',img)
-	k = cv2.waitKey(1) & 0xFF
-	if k == 27:
-		break
-	b = cv2.getTrackbarPos('brightness', WNAME) - TBMAX
-	s = cv2.getTrackbarPos('contrast', WNAME) - TBMAX
-	g = cv2.getTrackbarPos('gamma', WNAME) - TBMAX
-
-	print("b: " + str(b))
-
-	cv2.convertScaleAbs(img, img2,  b)
-	img = img2
-
-cv2.destroyAllWindows()
+	cv2.waitKey(0) 
+	cv2.destroyAllWindows()
