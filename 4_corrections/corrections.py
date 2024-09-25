added gamma with; need historgams & fix scaling

4_corrections/corrections.py
@@ -14,15 +14,27 @@ WNAME = 'Brightness, Contrast, Gamma'
 def bsg(event=0): 
 	b = cv2.getTrackbarPos('brightness', WNAME)
 	c = cv2.getTrackbarPos('contrast', WNAME)
-	g = cv2.getTrackbarPos('gamma', WNAME)
+	g = cv2.getTrackbarPos('gamma, (* 0.01)', WNAME)
 	effect = controller(img, b, c, g) 
+	print(b,c,g)
 	cv2.imshow(WNAME, effect)
-  
+
+def adjust_gamma(image, gamma=1.0):
+	# build a lookup table mapping the pixel values [0, 255] to
+	# their adjusted gamma values
+	invGamma = 1.0 / gamma
+	print(invGamma, gamma)
+	table = np.array([((i / 255.0) ** invGamma) * 255 for i in np.arange(0, 256)]).astype(np.uint8)
+	# apply gamma correction using the lookup table
+	return cv2.LUT(image, table)
+
 def controller(img, brightness=255, 
-               contrast=127, gamma = 0):
+               contrast=127, gamma = 100):
 
 	brightness = int((brightness - 0) * (255 - (-255)) / (510 - 0) + (-255)) 
 	contrast = int((contrast - 0) * (127 - (-127)) / (254 - 0) + (-127))
+	gamma = gamma * 0.01
+
 	if brightness != 0: 
 		if brightness > 0: 
 			shadow = brightness 
@@ -39,8 +51,11 @@ def controller(img, brightness=255,
 	if contrast != 0: 
 		Alpha = float(131 * (contrast + 127)) / (127 * (131 - contrast)) 
 		Gamma = 127 * (1 - Alpha) 
-		cal = cv2.addWeighted(cal, Alpha, cal, 0, Gamma) 
-	return cal 
+		cal = cv2.addWeighted(cal, Alpha, cal, 0, Gamma)
+
+	gamma_img = adjust_gamma(cal, gamma)
+	
+	return gamma_img 
 
 if __name__ == '__main__': 
 	img = cv2.imread(sys.argv[1])
@@ -51,8 +66,7 @@ if __name__ == '__main__':
 	
 	cv2.createTrackbar('brightness', WNAME, BMAX, BMAX*2, bsg)
 	cv2.createTrackbar('contrast', WNAME, CMAX, CMAX*2, bsg)
-	cv2.createTrackbar('gamma', WNAME, 0, BMAX*2, nothing)
-     
+	cv2.createTrackbar('gamma, (* 0.01)', WNAME, 100, 300, bsg)
 	bsg(0)
 
 	cv2.waitKey(0) 