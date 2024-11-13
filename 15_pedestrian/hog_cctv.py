ea19ab224be262fd755b5e5247cbcdcd3baa4f76
Andrey Rudometov <a.rudometov@postgrespro.ru>
2024-11-13 12:29:57 +0700

    parallel hog resized for 600

--- 15_pedestrian/hog_cctv.py
+++ 15_pedestrian/hog_cctv.py
@@ -2,12 +2,18 @@ import sys
 import cv2 as cv
 import imutils
 
+from multiprocessing.pool import ThreadPool
+from collections import deque
+
+from common import clock, draw_str, StatValue
+import video
+
 hog = cv.HOGDescriptor()
 hog.setSVMDetector(cv.HOGDescriptor_getDefaultPeopleDetector())
 
 # yt-dlp -f 136 https://youtu.be/GJNjaRJWVP8?si=STQl187QLl1B-Qia --username=oauth --password=""
 # ~/Videos/The CCTV People Demo 2 [GJNjaRJWVP8].mp4
-vid = cv.VideoCapture(sys.argv[1])
+cap = cv.VideoCapture(sys.argv[1])
 # 400
 minwid = int(sys.argv[2])
 
@@ -22,43 +28,86 @@ prob_low = 0.13
 prob_med = 0.3
 prob_high = 0.7
 
-st = 2
+preset = sys.argv[3]
+
+st = 4
 pd = 8
-sc = 1.04
+sc = 1.05
+
+if preset == "slow":
+	st = 2
+	sc = 1.04
+
+def process_frame(frame, t0):
+	original_width = frame.shape[1]
+	frame = imutils.resize(frame, width=min(minwid, frame.shape[1]))
+	frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+	(regions, weights) = hog.detectMultiScale(
+			frame_gray,
+			winStride=(st, st),
+			padding=(pd, pd),
+			scale=sc
+			)
+
+	for i, (x, y, w, h) in enumerate(regions):
+		prob = weights[i]
+		bcolor = bcolor_high
+		match prob:
+			case prob if prob < prob_low:
+				continue
+			case prob if prob_low <= prob < prob_med:
+				bcolor = bcolor_low
+			case prob if prob_med <= prob < prob_high:
+				bcolor = bcolor_med
+	
+		cv.rectangle(frame, (x, y), (x + w, y + h), bcolor, bsize)
+
+	frame = imutils.resize(frame, width=original_width)
+
+	return frame, t0
 
 cv.namedWindow(WNAME)
 
-while vid.isOpened():
-	ret, image = vid.read()
-	if ret:
-		image = imutils.resize(image, width=min(minwid, image.shape[1]))
+#threadn = cv.getNumberOfCPUs() - 4
+threadn = 6
+print("Threads: ", threadn)
+pool = ThreadPool(processes = threadn)
+pending = deque()
+
+latency = StatValue()
+frame_interval = StatValue()
+last_frame_time = clock()
+
+while True:
+	while len(pending) > 0 and pending[0].ready():
+		res, t0 = pending.popleft().get()
+		latency.update(clock() - t0)
 		
-		img_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-		(regions, weights) = hog.detectMultiScale(
-				img_gray,
-				winStride=(st, st),
-				padding=(pd, pd),
-				scale=sc
-				)
+		draw_str(res, (20, 20), "latency        :  %.1f ms" % (latency.value*1000))
+		draw_str(res, (20, 40), "frame interval :  %.1f ms" % (frame_interval.value*1000))
+		
+		cv.imshow(WNAME, res)
 
-		for i, (x, y, w, h) in enumerate(regions):
-			prob = weights[i]
-			bcolor = bcolor_high
-			match prob:
-				case prob if prob < prob_low:
-					continue
-				case prob if prob_low <= prob < prob_med:
-					bcolor = bcolor_low
-				case prob if prob_med <= prob < prob_high:
-					bcolor = bcolor_med
+	if len(pending) < threadn:
+		_ret, frame = cap.read()
+		t = clock()
+		frame_interval.update(t - last_frame_time)
+		last_frame_time = t
 			
-			cv.rectangle(image, (x, y), (x + w, y + h), bcolor, bsize)
-
-		cv.imshow(WNAME, image)
-		if cv.waitKey(25) & 0xFF == ord('q'):
-			break
-	else:
+		task = pool.apply_async(process_frame, (frame.copy(), t))
+		pending.append(task)
+		
+	ch = cv.waitKey(1)
+	if ch & 0xFF == ord('q'):
 		break
 
-vid.release()
+while len(pending) > 0:
+	if pending[0].ready:
+		res, t0 = pending.popleft().get()
+	else:
+		time.sleep(1)
+		
+
+cap.release()
 cv.destroyAllWindows()
